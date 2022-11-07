a910d736869db5f9ebbc7a6fe373488bba549bdb
ralph <ralph@localhost>
2022-11-07 23:59:15 +0100

    add api router

---
+++ README.md
@@ -0,0 +1,23 @@
+# Discdoor Gateway service
+
+This is the gateway server for Discdoor. Think of it as the backbone of the entire platform - this service decides which requests go to what subservices!
+
+Before using it, ensure all submodules are synced first! This is important since the gateway directly hosts the client, which is in a separate module!
+
+## System requirements
+
+* Microsoft Windows 10 or later.
+
+* NodeJS v16 or newer.
+
+* A valid MongoDB installation.
+
+## How to get started
+
+* Ensure submodules are synchronized and up-to-date.
+
+* Run `npm install` to install all dependencies.
+
+* Run `node index.js` to start the server.
+
+* Open a browser and navigate to `localhost:9000` to visit the local Discdoor instance.
--- index.js
+++ index.js
@@ -4,9 +4,17 @@ Main gateway source.
 
 const express = require('express');
 const app = express();
+const proxy = require('express-http-proxy');
 const cfg = require('./data/config.json');
+const apis = require('./data/api-mapping.json');
+const path = require('path');
+const libdd = require('libdd-node');
 
-app.get(`/api/${cfg.api.version}/ver`, (req, res)=>res.end(JSON.stringify({ version: cfg.api.version })));
+// Map all apis appropriately
+for(let apiMapping of apis)
+    app.use(`/${cfg.api.version}/${apiMapping.url}`, proxy(apiMapping.target));
+
+app.get(`/${cfg.api.version}/ver`, (req, res)=>res.end(JSON.stringify({ version: cfg.api.version })));
 
 app.listen(cfg.http.port, () => {
     console.log(`Gateway available at :${cfg.http.port}`);
