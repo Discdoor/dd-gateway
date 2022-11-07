c047ee0e608c35128b02eed4c68d8ba1bc8c992b
ralph <ralph@localhost>
2022-11-08 00:47:13 +0100

    remove api map

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ const express = require('express');
 const app = express();
 const proxy = require('express-http-proxy');
 const cfg = require('./data/config.json');
-const apis = require('./data/api-mapping.json');
 const path = require('path');
 const libdd = require('libdd-node');
-
-// Map all apis appropriately
-for(let apiMapping of apis)
-    app.use(`/${cfg.api.version}/${apiMapping.url}`, proxy(apiMapping.target));
 
 app.get(`/${cfg.api.version}/ver`, (req, res)=>res.end(JSON.stringify({ version: cfg.api.version })));
 
